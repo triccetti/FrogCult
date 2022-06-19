a303e5f9413b41310aaa62f13ce1301deedbed15
triccetti <t.riccetti4@live.com>
2022-06-19 16:16:15 -0700

    Added commissions form

--- scripts.js
+++ scripts.js
@@ -2,105 +2,114 @@
 // - (event listeners, forEach loop)
 
 // wait for the entire page to finish loading
-window.addEventListener('load', function() {
-	
-	// setTimeout to simulate the delay from a real page load
-	setTimeout(lazyLoad, 10);
-	
+window.addEventListener('load', function () {
+
+    // setTimeout to simulate the delay from a real page load
+    setTimeout(lazyLoad, 10);
+
 });
 
 const discordReportHook = "https://discord.com/api/webhooks/988160587173363762/PwmMGa7Casg8BjDVSKUZCfAF8_IEVJQaWmdDU3Scr7oZzZtHpKGcvYHE6efJJJHv5sGd";
 
 function lazyLoad() {
-	var card_images = document.querySelectorAll('.card-image');
-	
-	// loop over each card image
-	card_images.forEach(function(card_image) {
-		var image_url = card_image.getAttribute('data-image-full');
-		var content_image = card_image.querySelector('img');
+    var card_images = document.querySelectorAll('.card-image');
 
-        if(!content_image.classList.contains('leaf')) {
-            
+    // loop over each card image
+    card_images.forEach(function (card_image) {
+        var image_url = card_image.getAttribute('data-image-full');
+        var content_image = card_image.querySelector('img');
+
+        if (!content_image.classList.contains('leaf')) {
+
             // change the src of the content image to load the new high res photo
             content_image.src = image_url;
-            
+
             // listen for load event when the new photo is finished loading
-            content_image.addEventListener('load', function() {
+            content_image.addEventListener('load', function () {
                 // swap out the visible background image with the new fully downloaded photo
                 card_image.style.backgroundImage = 'url(' + image_url + ')';
                 // add a class to remove the blur filter to smoothly transition the image change
                 card_image.className = card_image.className + ' is-loaded';
             });
         }
-	});
+    });
 }
 
 function showCommissionForm() {
-    document.getElementById('commissionForm').style.display = ''; 
-}
-
-function showMap() { 
-    document.getElementById('map').style.display = ''; 
-    document.getElementById('join').style.display = 'none'; 
-    document.getElementById('report').style.display = 'none'; 
-}
-
-function showJoin() { 
-    document.getElementById('join').style.display = ''; 
-    document.getElementById('map').style.display = 'none'; 
-    document.getElementById('report').style.display = 'none'; 
+    document.getElementById('commissionForm').style.display = '';
+    document.getElementById('CommisionLink').style.display = 'none';
+    document.getElementById('Commissions').style.display = 'none';
 }
 
 
-function showReport() { 
-    document.getElementById('join').style.display = 'none'; 
-    document.getElementById('map').style.display = 'none'; 
-    document.getElementById('report').style.display = ''; 
+function showCommissionMenu() {
+    document.getElementById('Commissions').style.display = ''; 
+    document.getElementById('CommisionLink').style.display = '';
+    document.getElementById('commissionForm').style.display = 'none';
+}
+
+function showMap() {
+    document.getElementById('map').style.display = '';
+    document.getElementById('join').style.display = 'none';
+    document.getElementById('report').style.display = 'none';
+}
+
+function showJoin() {
+    document.getElementById('join').style.display = '';
+    document.getElementById('map').style.display = 'none';
+    document.getElementById('report').style.display = 'none';
 }
 
 
-function showForm() { 
-    document.getElementById('form-div').style.display=''; 
-    document.getElementById('reportSuccess').style.display='none';
+function showReport() {
+    document.getElementById('join').style.display = 'none';
+    document.getElementById('map').style.display = 'none';
+    document.getElementById('report').style.display = '';
 }
 
 
-function sendMinecraftReport() { 
+function showForm() {
+    document.getElementById('form-div').style.display = '';
+    document.getElementById('reportSuccess').style.display = 'none';
+}
+
+
+function sendMinecraftReport() {
     var name = document.getElementById('reportName').value;
-    if(name == "") {
+    if (name == "") {
         name = "Anonymous"
     }
 
     var issue = document.getElementById('reportIssue').value;
 
-    if(issue == null || issue == "") {
-        document.getElementById('issueText').style.display='';
+    if (issue == null || issue == "") {
+        document.getElementById('issueText').style.display = '';
     } else {
 
         var report = "Report made by: **" + name + "**\n" +
-        ">>> " +  issue;
+            ">>> " + issue;
 
 
 
         const request = new XMLHttpRequest();
-        request.open("POST", discordReportHook, true); 
+        request.open("POST", discordReportHook, true);
         request.setRequestHeader('Content-type', 'application/json');
- 
 
-        request.onreadystatechange = function() { // Call a function when the state changes.
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) { 
+
+        request.onreadystatechange = function () { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                 // successful
                 console.log("success!");
-                document.getElementById('reportSuccess').style.display='';
-                document.getElementById('issueText').style.display='none';
+                document.getElementById('reportSuccess').style.display = '';
+                document.getElementById('issueText').style.display = 'none';
             }
         }
         const params = {
             username: "Frog Cult Complaint Bot",
             avatar_url: "",
             content: report
-        } 
-        
+        }
+
         request.send(JSON.stringify(params));
         // after send clear values
         document.getElementById('reportIssue').value = "";
@@ -108,9 +117,9 @@ function sendMinecraftReport() {
 
 
         console.log("success!");
-        document.getElementById('form-div').style.display='none';
-        document.getElementById('issueText').style.display='none';
-        document.getElementById('reportSuccess').style.display='';
+        document.getElementById('form-div').style.display = 'none';
+        document.getElementById('issueText').style.display = 'none';
+        document.getElementById('reportSuccess').style.display = '';
     }
 }
- 
+
